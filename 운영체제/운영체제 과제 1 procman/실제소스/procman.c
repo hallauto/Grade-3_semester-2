323846d68cade4de96359407b4d0b02a2799f958
kimchangyu <kimchangyu@kimchangyu-15ND530-UX50K>
2015-09-24 13:13:27 +0900

    운영체제 과제 1 procman pass... 명칭 parse로 변경
    제목대로, pass를 parse로 변경했습니다.
    영어를 더 열심히 공부합시다.

--- 운영체제/운영체제 과제 1 procman/실제소스/procman.c
+++ 운영체제/운영체제 과제 1 procman/실제소스/procman.c
@@ -39,7 +39,7 @@ void read_config_file()
 	size_t size = 0; //getline함수를 쓰기위해서 존재하는 변수입니다. 이 안에 값이 쓰일 일은 없습니다.
 	read_new_line_letter();	//이 함수로 config파일의 줄 수를 확인하고 저장합니다.
 	input_string_array = calloc(line_many,sizeof(char *));
-	pass_str_array = calloc(line_many,sizeof(passed_string *));
+	parse_str_array = calloc(line_many,sizeof(parseed_string *));
 
 	int line_index;
 	for (line_index = 0; !feof(argv_file); line_index++) //한줄씩 읽고 파싱합니다.
@@ -47,6 +47,7 @@ void read_config_file()
 		getline(&input_string_array[line_index],&size,argv_file);
 		if (input_string_array[line_index][0] != '#' && strlen(input_string_array[line_index]) > 0)
 		{
+			printf("%s",input_string_array[line_index]);
 			//이 줄은 주석이 아닙니다. 또한 빈 줄도 아닙니다. 따라서 바로 파싱에 들어갑니다.
 			
 			//뭐하냐 몸통아 냉큼 파싱 함수 안 만들고! 뭐? 나보고 만들라고? 하여간 요즘 몸들은... 알겠다 strsep()가 한다!
@@ -61,7 +62,7 @@ void read_config_file()
 }
 
 /**
- * \n의 갯수 = 행의 갯수를 읽는 함수입니다. 이후에 이 행의 갯수 만큼 passed_string 구조체의 배열을 동적으로 할당할 것입니다.
+ * \n의 갯수 = 행의 갯수를 읽는 함수입니다. 이후에 이 행의 갯수 만큼 parseed_string 구조체의 배열을 동적으로 할당할 것입니다.
  */ 
 void read_new_line_letter()
 {
