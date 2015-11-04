b1e82070f3fb58e56b57223ec2c6c1970e03f4e5
kimchangyu <cdit2464@gmail.com>
2015-11-05 04:09:55 +0900

    운영체제 과제 3 sem 구조체 제작 및 생성자 완성
    구조체와 생성자를 만들었습니다.

--- 운영체제/운영체제 과제 3 sem/실제코드/sem.c
+++ 운영체제/운영체제 과제 3 sem/실제코드/sem.c
@@ -7,37 +7,74 @@
 #include "sem.h"
 #include <stdlib.h>
 
+typedef struct wait_semaphore
+{
+	tsem_t * mutext
+
+}wait_que;
+
+/*
+이 구조체는 insert_value를 조작할 때 상호배제가 보장되며, 이는 mutex로 구현되어있습니다.
+*/
 struct test_semaphore
 {
- 	pthread_mutex_t mutext_value; //insert_value의 연산을 원자화 하기위한 mutext 구조체입니다.
- 	int insert_value; //입력된 세마포어 값입니다. 이 값을 기준으로 프로그램이 동작합니다.
+	pthread_cond_t cond_value; //test관련 연산들의 대기열 관리를 위한 조건변수 구조체입니다.
+	pthread_mutex_t cond_mutex_value; //test관련 연산들의 대기열 관리를 위한 mutex_t 구조체입니다.
+
+ 	pthread_mutex_t mutex_value; //insert_value의 연산을 원자화 하기위한 mutex_t 구조체입니다.
+ 	int insert_value; //입력된 세마포어 값입니다. 이 값을 조작하는 부분이 원자화 되어야합니다. 또, 이 값이 0이하라면 lock이 걸려야합니다.
 };
 
 tsem_t * tsem_new (int value)
 {
-  /* not yet implemented. */
+	tsem_t * new_sem = calloc(1,sizeof(tsem_t)); //동적으로 메모리를 할당합니다.
+	new_sem->mutex_value = PTHREAD_MUTEX_INITIALIZER; //pthread_mutex_t는 기본 정적 인자를 이용해서 만듭니다.
+	new_sem->insert_value = value;
 
-  return NULL;
+
+  return new_sem;
 }
 
 void tsem_free (tsem_t *sem)
 {
-  /* not yet implemented. */
+	free(sem);
 }
 
 void tsem_wait (tsem_t *sem)
 {
-  /* not yet implemented. */
+	/*순서대로 다음과 같이 이루어져야합니다.
+	1. 먼저 insert_value가 원자적으로 조작되도록 mutex를 이용합니다.
+	2. 성공적으로 mutex의 lock을 얻었다면 이제 insert_value의 값을 검사합니다.
+	3-1. 만약 0이하라면 -1을 하고 cond_value를 이용해서 lock에 들어갑니다.
+	3-2. 만약 0 초과라면 -1을 하고 그대로 프로그램은 동작합니다.
+	*/
+	int status; //mutex를 통해 lock을 획득하려고 시도한 결과 값입니다.
+
+	status = pthread_mutex_lock(&(sem->mutex_value));
+	
+	//critical section(임계 영역)
+	if ()
+	//critical section(임계 영역) 끝
+
 }
 
 int tsem_try_wait (tsem_t *sem)
 {
-  /* not yet implemented. */
-
-  return 1;
+	/*순서대로 다음과 같이 이루어져야합니다.
+	1. 먼저 insert_value가 원자적으로 조작되도록 mutex를 이용합니다.
+	2. 성공적으로 mutex의 lock을 얻었다면 이제 insert_value의 값을 검사합니다.
+	3-1. 만약 0이하라면 -1을 하고 cond_value를 이용해서 lock에 들어가...는게 아니고 1을 반환합니다.
+	3-2. 만약 0 초과라면 -1을 하고 0을 반환합니다.
+	*/
+	return 1;
 }
 
 void tsem_signal (tsem_t *sem)
 {
-  /* not yet implemented. */
+	/*순서대로 다음과 같이 이루어져야합니다.
+	1. 먼저 insert_value가 원자적으로 조작되도록 mutex를 이용합니다.
+	2. 성공적으로 mutex의 lock을 얻었다면 이제 insert_value의 값을 검사합니다.
+	3-1. 만약 0 이하라면 -1을 하고 cond_value를 이용해서 lock에 들어가...는게 아니고 1을 반환합니다.
+	3-2. 만약 0 초과라면 
+	*/
 }
