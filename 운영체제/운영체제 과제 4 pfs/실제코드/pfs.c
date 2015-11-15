2b171b8b42aa93052e283547314190efd8395d38
kimchangyu <cdit2464@gmail.com>
2015-11-16 01:27:02 +0900

    운영체제 과제 4 pfs popen함수 조사 필요
    popen함수와 ps -o pid= 커맨드를 이용하면 현재 실행중인 프로세스의 pid를 전부 알 수 있습니다. 문제는 이렇게 얻은
    pid로 proc파일에 접근하는 함수를 만드는 것입니다.

--- 운영체제/운영체제 과제 4 pfs/실제코드/pfs.c
+++ 운영체제/운영체제 과제 4 pfs/실제코드/pfs.c
@@ -10,10 +10,14 @@
 #include <errno.h>
 #include <fcntl.h>
 
-int make_proc_id;
-char* make_proc_name;
-time_t make_proc_time;
-time_t access_proc_time;
+int make_proc_id; //생성된 프로세스의 아이디입니다.
+char* make_proc_name; //생성된 프로세스의 이름입니다.
+char* make_proc_file_name; //위의 두 변수를 합쳐서 만든 파일 이름입니다.
+int make_proc_owner; //생성된 프로세스의 사용자 = 파일 소유자입니다.
+time_t make_proc_time; //생성된 프로세스의 생성 시간입니다.
+time_t access_proc_time; //생성된 프로세스의 최근 접근 시간입니다.
+
+int* pid_array; //pid를 저장할 배열입니다. 모든 pid들이 여기에 저장됩니다.
 
 static int pfs_getattr (const char  *path,
                         struct stat *stbuf)
@@ -51,6 +55,18 @@ static int pfs_unlink (const char *path)
 {
   /* not yet implemented */
   return -ENOENT;
+}
+
+/*
+proc를 제대로 이용하려면 pid를 획득해야합니다. 그 함수가 이 함수입니다.
+다음과 같은 순서와 커맨드를 이용해서 작동합니다.
+1.ps -o pid= 을 이용해서 현재 실행중인 프로세스의 pid를 전부 구합니다. 이 값은 \n으로 구분됩니다.
+2.각 pid들을 구해서 int값으로 만들어(atoi) 배열에 저장합니다.
+*/
+int get_proc_pid()
+{
+  //ps커맨드를 실행하는 것 뿐만아니라 그 결과 값을 얻어야함으로 popen함수를 이용합니다.
+  char* pid_string = popen("ps -o pid=");
 }
 
 static struct fuse_operations pfs_oper =
