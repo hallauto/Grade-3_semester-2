63a3fa31d10d494404c048452fb46105ba77ecbf
kimchangyu <cdit2464@gmail.com>
2015-11-17 23:25:14 +0900

    운영체제 과제 4 pfs ls -l을 이용한 버전
    ps는 과제에 사용할 수 없다는 것을 확인했습니다. 그러니 이제부터는 /proc 디렉토리에서 딱 pid만 읽어서 정리해놓읍시다.

--- 운영체제/운영체제 과제 4 pfs/실제코드/pfs.c
+++ 운영체제/운영체제 과제 4 pfs/실제코드/pfs.c
@@ -10,12 +10,14 @@
 #include <errno.h>
 #include <fcntl.h>
 
-int make_proc_id; //생성된 프로세스의 아이디입니다.
-char* make_proc_name; //생성된 프로세스의 이름입니다.
-char* make_proc_file_name; //위의 두 변수를 합쳐서 만든 파일 이름입니다.
-int make_proc_owner; //생성된 프로세스의 사용자 = 파일 소유자입니다.
-time_t make_proc_time; //생성된 프로세스의 생성 시간입니다.
-time_t access_proc_time; //생성된 프로세스의 최근 접근 시간입니다.
+int* proc_pid_names
+int proc_pid; //검색된 프로세스의 아이디입니다.
+char* proc_name; //검색된 프로세스의 이름입니다.
+char* proc_file_name; //위의 두 변수를 합쳐서 만든 파일 이름입니다.
+int proc_owner; //검색된 프로세스의 사용자 = 파일 소유자입니다.
+time_t maked_proc_time; //검색된 프로세스의 검색 시간입니다.
+time_t access_proc_time; //검색된 프로세스의 최근 접근 시간입니다.
+time_t modifi_proc_time; //검색된 프로세스의 수정된 시간입니다.
 
 int* pid_array; //pid를 저장할 배열입니다. 모든 pid들이 여기에 저장됩니다.
 
@@ -48,7 +50,34 @@ static int pfs_readdir (const char            *path,
                         struct fuse_file_info *fi)
 {
   /* not yet implemented */
+  /*
+  여기에서 각 pid를 이용해서 정보를 얻고 이를 filler에 저장해야합니다.
+  이를 위해서 각각의 함수를 만들어야합니다.
+  int pid_accesstime() :접속 시간을 반환합니다.
+  int pid_modifitime() :수정 시간을 반환합니다.
+  int pid_createtime() :생성 시간을 반환합니다.
+  char* pid_cmdline() :실행시의 커맨드 라인을 읽습니다.
+  char* pid_proc_name() :프로세스의 이름을 읽습니다.
+  */
   return -ENOENT;
+}
+
+static char* pid_proc_name()
+{
+
+}
+
+/*
+  pid와 프로세스 이름을 합쳐서 파일 이름을 만듭니다.
+*/
+static char* make_pid_proc_file_name()
+{
+
+}
+
+static int pid_accesstime(const char)
+{
+
 }
 
 static int pfs_unlink (const char *path)
@@ -60,13 +89,13 @@ static int pfs_unlink (const char *path)
 /*
 proc를 제대로 이용하려면 pid를 획득해야합니다. 그 함수가 이 함수입니다.
 다음과 같은 순서와 커맨드를 이용해서 작동합니다.
-1.ps -o pid= 을 이용해서 현재 실행중인 프로세스의 pid를 전부 구합니다. 이 값은 \n으로 구분됩니다.
+
 2.각 pid들을 구해서 int값으로 만들어(atoi) 배열에 저장합니다.
 */
 int get_proc_pid()
 {
-  //ps커맨드를 실행하는 것 뿐만아니라 그 결과 값을 얻어야함으로 popen함수를 이용합니다.
-  char* pid_string = popen("ps -o pid=");
+  pid_many = popen("ls /proc | grep ^[0-9]*$ | wc -l");
+  char * pid_string = popen("ls /proc | grep ^[0-9]*$");
 }
 
 static struct fuse_operations pfs_oper =
