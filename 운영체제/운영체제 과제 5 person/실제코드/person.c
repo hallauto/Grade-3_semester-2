c43f1009842a71319bf851e046e10cca1f41d717
kimchangyu <cdit2464@gmail.com>
2015-11-29 01:36:17 +0900

    운영체제 과제 5 person check_file_exist 함수
    기존에 파일의 존재여부를 파악하는 모듈을 함수로 분리시 켰습니다. 또한, print_get_attr을 제작하고 있습니다. 문제는
    포인터는 쉽게 얻어도, 이 다음에 '형식에 따라 출력방식이 달라지는 것을 구현하는 부분입니다.

--- 운영체제/운영체제 과제 5 person/실제코드/person.c
+++ 운영체제/운영체제 과제 5 person/실제코드/person.c
@@ -16,7 +16,7 @@ int map_pointer; //매핑된 주소 포인터입니다.
  * 주어진 매개변수와 지정된 값을 활용해서 메모리 맵을 지정합니다.
  * 기본적으로 프로그램은 
  */
-static void* allocate_mmap(int file_descriptor)
+void* allocate_mmap(int file_descriptor)
 {
 	if (file_descriptor < 0)
 	{
@@ -33,6 +33,40 @@ static void* allocate_mmap(int file_descriptor)
 	}
 	
 	return map_pointer;
+}
+
+/*
+  * 해당하는 이름의 파일이 존재하는지 파악합니다.
+  * 만약 없다면 새로 생성하고 Person구조체 크기만큼 0x00 = 0으로 채웁니다.
+  */
+void check_file_exist(const char* file_name)
+{
+  int empty_index; //새로 생성할때 이용할 인덱스입니다.
+  if (access(file_name, F_OK) == -1) //먼저 해당 파일이 존재하는지 확인합니다. 만약 존재하지 않으면 새로 생성하고 person_size만큼 0을 입력합니다.
+  {
+    file_pointer = fopen(file_name,"w+");
+    empty_array = calloc(person_size+1, sizeof(char));
+    for (empty_index = 0; empty_index < person_size + 1; empty_index++)
+    {
+      empty_array[empty_index] = 0;
+    }
+    fwrite(empty_array,1,person_size,file_pointer);
+    fclose(file_pointer);
+  }
+
+
+  return;
+}
+
+/*
+  * 'attr_name 값 = 지정된 속성 이름' 의 값을 매핑된 파일에서 가져와 출력합니다.
+  * const char *attr_name: 지정된 속성 이름입니다.
+  */
+static void print_get_attr(const char *attr_name)
+{
+  int attr_pointer = NULL; //속성의 포인터입니다.
+
+  attr_pointer = person_get_offset_of_attr (attr_name); //먼저 attr_name을 이용해서 person구조체의 멤버를 파일에서 가져옵니다.
 }
 
 static void
@@ -95,13 +129,6 @@ main (int    argc,
 	attr_name = argv[optind];
 
 	/* not yet implemented */
-	if (access(file_name, F_OK) == -1) //먼저 해당 파일이 존재하는지 확인합니다. 만약 존재하지 않으면 새로 생성하고 person_size만큼 0을 입력합니다.
-	{
-		file_pointer = fopen(file_name,"w+");
-		empty_array = calloc(person_size+1, sizeof(char));
-		fwrite(empty_array,1,person_size,file_pointer);
-		fclose(file_pointer);
-	}
 	file_pointer = fopen(file_name,"w+");
 	if (file_pointer == NULL)
 	{
@@ -115,7 +142,8 @@ main (int    argc,
 	
 	if (set_value == NULL)
 	{
-		
+    print_get_attr(attr_name);
+    
 	}
 	
 
