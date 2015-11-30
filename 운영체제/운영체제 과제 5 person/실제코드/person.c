fa5e515e4e50495dd0cac59656f8d793b438ef5a
kimchangyu <kimchangyu@kimchangyu-15ND530-UX50K>
2015-11-30 18:08:16 +0900

    운영체제 과제 5 person watchers 배열 등록 및 삭제 함수 제작
    대기 모드로 작동한 프로그램이 pid를 watchers에 저장하고, 종료될 때 삭제하도록 만들었습니다. 또한 파일과 메모리의
    싱크가 정상적으로 작동한다는 것도 덤으로 알았습니다.

--- 운영체제/운영체제 과제 5 person/실제코드/person.c
+++ 운영체제/운영체제 과제 5 person/실제코드/person.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 
 int* map_pointer = NULL; //매핑된 주소 포인터입니다.
+int watchers_index; //watchers 배열의 인덱스입니다. 이후에 대기 모드 프로세스가 종료될 때, 이 부분을 0으로 변환합니다.
 
 /*
  * 주어진 매개변수와 지정된 값을 활용해서 메모리 맵을 지정합니다.
@@ -40,7 +41,7 @@ void * allocate_mmap(int file_descriptor)
 void check_file_exist(const char* file_name)
 {
 	FILE * file_pointer = NULL; //파일이 존재하지 않을 때, 새로 파일을 생성하고 내용을 초기화할 때 쓸 FILE 포인터입니다.
-	char* empty_array; //파일을 초기화할 떄 쓸 배열입니다.
+	int* empty_array; //파일을 초기화할 떄 쓸 배열입니다.
 	
 	//먼저 해당 파일이 존재하는지 확인합니다. 만약 존재하지 않으면 새로 생성하고 sizeof(Person)만큼 0을 입력합니다.
 	if (access(file_name, F_OK) == -1)
@@ -61,9 +62,94 @@ void check_file_exist(const char* file_name)
  */
 static void print_get_attr(const char *attr_name)
 {
-	int attr_pointer = NULL; //속성의 포인터입니다.
+	int attr_offset = 0; //속성의 오프셋입니다.
 
-	attr_pointer = person_get_offset_of_attr (attr_name); //먼저 attr_name을 이용해서 person구조체의 멤버를 파일에서 가져옵니다.
+	attr_offset = person_get_offset_of_attr (attr_name); //먼저 attr_name을 이용해서 person구조체의 멤버를 파일에서 가져옵니다.
+	
+	/*
+	 * 최종 결과물에서는 이 함수를 이용해서 출력해야합니다. 하지만 일단은 디버깅 및 개발의 용이를 위해 각 속성 이름에 따라 출력하도록 만들었습니다.
+	if (person_attr_is_integer(attr_name))
+	{
+		
+	}
+	*/
+	if (!strcmp (attr_name, "name"))
+	{
+		printf("name: %s\n", (char *)map_pointer);
+		return;
+	}
+	if (!strcmp (attr_name, "age"))
+	{
+		printf("age: %d\n", *(map_pointer + attr_offset));
+		return;
+	}
+	if (!strcmp (attr_name, "gender"))
+	{
+		printf("gender: %d\n", *(map_pointer + attr_offset));
+	}
+	if (!strcmp (attr_name, "phone"))
+	{
+		printf("name: %s\n", (char *)map_pointer + attr_offset);
+		return;
+	}
+	if (!strcmp (attr_name, "homepage"))
+	{
+		printf("name: %s\n", (char *)map_pointer + attr_offset);
+		return;
+	}
+	if (!strcmp (attr_name, "twitter"))
+	{
+		printf("name: %s\n", (char *)map_pointer + attr_offset);
+		return;
+	}
+	if (!strcmp (attr_name, "facebook"))
+	{
+		printf("name: %s\n", (char *)map_pointer + attr_offset);
+		return;
+	}
+}
+
+int find_empty_index()
+{
+	pid_t* watchers_pointer = NULL; //watchers배열의 시작 주소입니다.
+	int find_index = 0; //watchers배열의 인덱스입니다.
+	
+	watchers_pointer = map_pointer; //watchers_pointer는 Person 구조체의 첫번째 멤버변수입니다. 따라서 시작주소가 곧 watchers 배열의 시작주소입니다. 
+	
+	while (find_index < NOTIFY_MAX)
+	{
+		if (*(watchers_pointer + find_index * sizeof(pid_t)) == 0)
+		{
+			*(watchers_pointer + find_index * sizeof(pid_t)) = getpid();
+			printf("watchers [%d] = %d\n",find_index, *(watchers_pointer + find_index * sizeof(pid_t)));
+			return find_index;
+		}
+		
+		find_index++;
+	}
+	
+	//반복문을 벗어났다는 것은 현재 비어있는 공간이 없다는 뜻입니다. 따라서 요구사항대로 0번에 저장합니다.
+	find_index = 0;
+	*watchers_pointer = getpid();
+	
+	printf("watchers [%d] = %d\n",find_index, *(watchers_pointer + find_index * sizeof(pid_t)));
+	return 0;
+}
+
+/*
+ * 대기모드 프로세스가 종료될 때, 해당 watchers 배열 인덱스의 값을 0으로 되돌립니다.
+ */
+void delete_watchers_index()
+{
+	
+	if (*(map_pointer + watchers_index * sizeof(pid_t)) == 0)
+	{
+		return;
+	}
+	else
+	{
+		*(map_pointer + watchers_index * sizeof(pid_t)) = 0;
+	}
 }
 
 static void
@@ -139,10 +225,26 @@ main (int    argc,
 	{
 		printf("메모리 매핑중에 문제가 발생했습니다.\n");
 		return -1;
+	}	
+	
+	//watch_mode가 1이면 이 프로세스의 pid를 watchers 배열에 저장해야합니다. 또, 시그너 핸들러를 등록해야합니다. 이후에 이 프로그램은 무한 루프를 돕니다.
+	if (watch_mode == 1)
+	{
+		watchers_index = find_empty_index(); //먼저 pid를 watchers에 저장하기 위해 빈 공간의 인덱스를 찾습니다.
+		
+		
+		
+		delete_watchers_index();
+		return 0;
 	}
 	
 	if (set_value == NULL) //set_value가 존재하는지 검사합니다. 만약 존재하지 않으면 지정한 속성 값을 출력합니다.
 	{
+		if (attr_name == NULL)
+		{
+			print_usage(argv[0]);
+			return -1;
+		}
 		print_get_attr(attr_name);
 	}
 	
